9f86f0956bc981cb60b0171faf0ddf6c8c08f3c0
Sina-Ebadollahi <Ebadollahisina@Gmail.com>
2022-07-13 07:22:12 +0430

    added top side cover color

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <div className="HomeContainer">
         <div className="MountainContainer">
-            
+            <div className="MountainContainerCover"></div>
         </div>
         <div className="BlankDivContainer">
 
