de297d53dd6fe07119bcfc5230317d4126081bda
Sina-Ebadollahi <Ebadollahisina@Gmail.com>
2022-07-13 07:15:15 +0430

    Dividing background to 2 sides and coding top side

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './App.css';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./App.css";
+import Home from "./Pages/Home/Home";
 
 function App() {
-  return <div className="App"></div>;
-
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,7 +2,12 @@ import './Home.css'
 export default function Home() {
   return (
     <div className="HomeContainer">
-        
+        <div className="MountainContainer">
+            
+        </div>
+        <div className="BlankDivContainer">
+
+        </div>
     </div>
   )
 }
