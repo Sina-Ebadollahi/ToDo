654ba992bd1d251c6233723e0ed7ed6532565bc3
Sina-Ebadollahi <Ebadollahisina@Gmail.com>
2022-07-15 02:31:55 +0430

    alomst completed item component

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,18 +1,25 @@
+import { useState } from 'react';
 import { RoundedRadioInput } from '../Input/InputCard';
 import './Item.css'
 
 
 
 
-export const 
 export default function Item(props) {
-    const { taskDetail, taskIsFinished } = props;
+    const { taskDetail, taskIsFinished, taskID } = props;
+    const [isThisItemHovered, setIsThisItemHovered] = useState(false);
   return (
-    <div className="item-container">
+    <div key={taskID} className="item-container" onMouseEnter={() => setIsThisItemHovered(true)} onMouseLeave={() => setIsThisItemHovered(false)}>
         <RoundedRadioInput isChecked={taskIsFinished} />
         <div className="task-title-container">
-            <h1 className="title-header">{taskDetail}</h1>
+            <h1 className="title-header" title={taskDetail}>{taskDetail}</h1>
         </div>
+        {isThisItemHovered && (
+            // div onClick deletes task
+            <div className="delete-task-btn" onClick={(e) => {}}>
+                <div className="close"></div>
+            </div>
+        )}
     </div>
   )
 }
