33902a9fc90e19c20aeb2b9f9abb7722b446a0a0
Sina-Ebadollahi <Ebadollahisina@Gmail.com>
2022-07-14 09:22:32 +0430

    seprated Rounded checkbox input from others

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import './Home.css'
 import React from 'react'
 import Frame from "../../components/Frame/Frame";
+import InputCard from '../../components/Input/InputCard';
 
 export default function Home() {
   return (
@@ -10,7 +11,7 @@ export default function Home() {
         </div>
         <div className="BlankDivContainer">
             <div>
-                <Frame className={"input_frame"} shadow={1}>this is input box</Frame>
+                <Frame className={"input_frame"} shadow={1}><InputCard /></Frame>
                 <Frame className={"body_frame"} shadow={1}>this is boy frame</Frame>
             </div>
 
--- src/components/Input/InputCard.jsx
+++ src/components/Input/InputCard.jsx
@@ -1,9 +1,21 @@
 import './InputCard.css'
+import React from 'react'
+
+// Rounded Radio
+export const RoundedRadioInput = () => {
+  return(
+    <>
+      <input type="checkbox" name="" id="" className='radio_input' />
+    </>
+  )
+}
 export default function InputCard() {
   return (
     <div className="InputCard">
       <div className="radio_input_container">
-        
+        <div className="input_wrapper">
+          <RoundedRadioInput />
+        </div>
       </div>
       <div className="text_input_container"></div>
     </div>
