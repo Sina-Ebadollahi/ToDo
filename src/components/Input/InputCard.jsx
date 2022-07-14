16b36c601eee95ec0b1f5d62f4e96a5dd6cb5303
Sina-Ebadollahi <Ebadollahisina@Gmail.com>
2022-07-14 08:17:20 +0430

    initializing main container of input component

---
+++ src/components/Input/InputCard.jsx
@@ -0,0 +1,11 @@
+import './InputCard.css'
+export default function InputCard() {
+  return (
+    <div className="InputCard">
+      <div className="radio_input_container">
+        
+      </div>
+      <div className="text_input_container"></div>
+    </div>
+  )
+}
