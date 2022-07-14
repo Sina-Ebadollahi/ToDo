5ec0f0406c8facdc94ee5f67280e1ea7a035fadd
Sina-Ebadollahi <Ebadollahisina@Gmail.com>
2022-07-14 07:45:02 +0430

    minor bugfixes & finalizing Home page routes

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./App.css";
 import Home from "./Pages/Home/Home";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   return (
-    <>
-      <Router>
+    <React.Fragment>
+      <BrowserRouter>
         <Routes>
-          <Route path={"/" || "/Home"} element={<Home />} />
+          <Route path={"/"} element={<Home />} />
+          <Route path={"/Home"} element={<Home />} />
           {/* 404 not found page */}
           {/* <Route path='*' element={}/> */}
         </Routes>
-      </Router>
-    </>
+      </BrowserRouter>
+    </React.Fragment>
+    // </div>
   );
 }
 
