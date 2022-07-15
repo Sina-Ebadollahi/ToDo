8053f60aeb43e7d7fa0d6caea14ced3880087141
Sina-Ebadollahi <Ebadollahisina@Gmail.com>
2022-07-16 00:53:55 +0430

    structre of page finished

--- src/Pages/Signup/Signup.jsx
+++ src/Pages/Signup/Signup.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 import './Signup.css'
+export const GetInfoAuthPage = ({}) => {
+    return(
+        <>
+
+        </>
+    )
+}
+export const TermsOfService = ({reverseValueOfIsTermAccepted}) => {
+    return(
+        <div className="fc">
+            <input onChange={(e) => reverseValueOfIsTermAccepted()} type="checkbox" name="" id="" />
+            <p>I have read and agree to the <span className="term-link">Terms of Service</span></p>
+        </div>
+    )
+}
 export default function Signup() {
+    const [isTermAccepted, setIsTermAccepted] = useState(false);
+    const reverseValueOfIsTermAccepted = () => {
+        setIsTermAccepted(!isTermAccepted);
+    }
   return (
     <div className="signup-container">
+        <form className="form">
+            <div className="auth-header-wrapper fc">
+                <h1 className="auth-header">Create account</h1>
+                <h4 className="login-header">Already have an account ? <Link to="/Login"> Sign in</Link></h4>
+            </div>
+            <div className="user-info-wrapper fc">
+
+            </div>
+            <button className="auth-btn fc">Sign up</button>
+            <TermsOfService reverseValueOfIsTermAccepted={reverseValueOfIsTermAccepted} />
+        </form>
+
     </div>
   )
 }
