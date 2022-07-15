cebc822a63dec3ba4b503517b84a1117883236ac
Sina-Ebadollahi <Ebadollahisina@Gmail.com>
2022-07-16 00:32:13 +0430

    setting up routes for signup and signin

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import "./App.css";
 import Home from "./Pages/Home/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./Pages/Login/Login";
+import Signup from "./Pages/Signup/Signup";
 function App() {
   return (
     <React.Fragment>
@@ -10,6 +12,8 @@ function App() {
         <Routes>
           <Route path={"/"} element={<Home />} />
           <Route path={"/Home"} element={<Home />} />
+          <Route path={"/Signup"} element={<Signup />} />
+          <Route path={"/Login"} element={<Login />} />
           {/* 404 not found page */}
           {/* <Route path='*' element={}/> */}
         </Routes>
