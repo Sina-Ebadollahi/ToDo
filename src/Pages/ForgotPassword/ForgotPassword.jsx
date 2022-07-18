fa2058a5b40eac9a60f314832da47980b3237bc5
Sina-Ebadollahi <Ebadollahisina@Gmail.com>
2022-07-18 12:21:53 +0430

    initializing Forgot password page

---
+++ src/Pages/ForgotPassword/ForgotPassword.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './ForgotPassword.css'
+export default function ForgotPassword() {
+  return (
+    <section className="signup-container">
+        <form className="form">
+            
+        </form>
+    </section>
+  )
+}
