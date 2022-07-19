70275540d454540475f1c63670dabbab0a250d45
Mreba <Ebadollahisina@gmail.com>
2022-07-19 09:59:10 +0430

    updating reducer

---
+++ src/Reducer/RootReducer.js
@@ -0,0 +1,33 @@
+import { useReducer } from "react";
+
+export default function AppData() {
+  let defaultState = {
+    authInfo: {
+      isLoggedIn: false,
+      todos: [],
+      isUserEmailConfirmed: false,
+    },
+    userInfo: {
+      fName: "",
+      lName: "",
+      email: "",
+      password: "",
+    },
+  };
+  const rootReducer = useReducer((state = defaultState, action) => {
+    switch (action.type) {
+      case "LOG_IN":
+        return {
+          ...state,
+          authInfo: action.payload,
+        };
+      case "SIGN_UP":
+        return {
+          ...state,
+          userInfo: action.payload,
+        };
+      default:
+        return state;
+    }
+  });
+}
