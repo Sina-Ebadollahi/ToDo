7d515e1d3622529cec07d93a52bcc5bc5f06ffa8
Sina-Ebadollahi <Ebadollahisina@Gmail.com>
2022-07-24 09:12:16 +0430

    editing some codes

--- src/Hooks/useData.js
+++ src/Hooks/useData.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { state, dispatch } from "../Reducer/RootReducer";
+// import { state, dispatch } from "../Reducer/RootReducer";
 export default function useData() {
-  const handleSignUpUpdate = (updatedData) => {
-    dispatch({ type: "SIGN_UP", payload: updatedData });
-  };
-  const handleSignInUpdate = (updatedData) => {
-    dispatch({ type: "SIGN_IN", payload: updatedData });
-  };
-  return {
-    handleSignUpUpdate,
-    handleSignInUpdate,
-  };
+  // const handleSignUpUpdate = (updatedData) => {
+  //   dispatch({ type: "SIGN_UP", payload: updatedData });
+  // };
+  // const handleSignInUpdate = (updatedData) => {
+  //   dispatch({ type: "SIGN_IN", payload: updatedData });
+  // };
+  // return {
+  //   handleSignUpUpdate,
+  //   handleSignInUpdate,
+  // };
 }
